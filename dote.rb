0ac6a72238213ca13d7b7805c194af776442abef
nori417 <haruma.0417.nori@gmail.com>
2025-10-28 14:23:01 +0900

    Add version 1.0.0 to dote.rb

--- dote.rb
+++ dote.rb
@@ -3,6 +3,7 @@ class Dote < Formula
   homepage "https://github.com/nori417/homebrew-dote"
   url "https://raw.githubusercontent.com/nori417/homebrew-dote/main/dote.c"
   sha256 "e9add301e73c72f6e7a2951d49dc8c4b9c2cd06b62127952a65f03841a44461b"
+  version "1.0.0"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
